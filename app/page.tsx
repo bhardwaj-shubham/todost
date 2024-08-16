31f097a7a0200db59195cd351d5aed8c36dce020
Shubham Bhardwaj <shubhardwaj10@gmail.com>
2024-08-16 14:23:35 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <h1>Todost</h1>
+        <p>This is AI Based Task Management App</p>
+      </div>
+    </main>
+  );
+}

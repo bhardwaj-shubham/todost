dbb8c3faf526566ebd276f86e4dd89c01595131c
Shubham Bhardwaj <shubhardwaj10@gmail.com>
2024-08-18 10:49:32 +0530

    chore: Add Convex authentication configuration and dependencies

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,36 @@
 "use client";
 
-import { ReactNode } from "react";
-import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { ReactNode, useMemo } from "react";
+import { ConvexProviderWithAuth, ConvexReactClient } from "convex/react";
 import { Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
+
+import { SessionProvider, useSession } from "next-auth/react";
 
 const convexClient = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+function useAuth() {
+  const { data: session, update } = useSession();
+
+  return useMemo(
+    () => ({
+      isLoading: false,
+      isAuthenticated: session !== null,
+      fetchAccessToken: async ({
+        forceRefreshToken,
+      }: {
+        forceRefreshToken: boolean;
+      }) => {
+        if (forceRefreshToken) {
+          const session = await update();
+
+          return session?.convexToken ?? null;
+        }
+      },
+    }),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [JSON.stringify(session?.user)]
+  );
+}
 
 export default function Provider({
   children,
@@ -16,7 +41,10 @@ export default function Provider({
 }) {
   return (
     <SessionProvider session={session}>
-      <ConvexProvider client={convexClient}>{children}</ConvexProvider>;
+      <ConvexProviderWithAuth client={convexClient} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithAuth>
+      ;
     </SessionProvider>
   );
 }

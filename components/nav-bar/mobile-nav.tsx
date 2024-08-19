6e79d4cd211a47c7945b1e79779dc2b22b4e53a8
Shubham Bhardwaj <shubhardwaj10@gmail.com>
2024-08-19 12:51:34 +0530

    chore: Refactor the mobile component

--- components/nav-bar/mobile-nav.tsx
+++ components/nav-bar/mobile-nav.tsx
@@ -20,17 +20,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import {
-  CircleUser,
-  Home,
-  LineChart,
-  Menu,
-  Package,
-  Package2,
-  Search,
-  ShoppingCart,
-  Users,
-} from "lucide-react";
+import { CircleUser, Menu, Search } from "lucide-react";
 
 export default function MobileNav() {
   return (
@@ -109,25 +99,6 @@ export default function MobileNav() {
           </DropdownMenuContent>
         </DropdownMenu>
       </header>
-      <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-        <div className="flex items-center">
-          <h1 className="text-lg font-semibold md:text-2xl">Inventory</h1>
-        </div>
-        <div
-          className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
-          x-chunk="dashboard-02-chunk-1"
-        >
-          <div className="flex flex-col items-center gap-1 text-center">
-            <h3 className="text-2xl font-bold tracking-tight">
-              You have no products
-            </h3>
-            <p className="text-sm text-muted-foreground">
-              You can start selling as soon as you add a product.
-            </p>
-            <Button className="mt-4">Add Product</Button>
-          </div>
-        </div>
-      </main>
     </div>
   );
 }

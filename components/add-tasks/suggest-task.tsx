1db76d7de835b55ee631aafc3157ecb65cdb4f65
Shubham Bhardwaj <shubhardwaj10@gmail.com>
2024-08-31 13:11:14 +0530

    feat: Add SuggestMissingTasks component for suggesting missing tasks

---
+++ components/add-tasks/suggest-task.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+import { Id } from "@/convex/_generated/dataModel";
+import { useAction } from "convex/react";
+import { api } from "@/convex/_generated/api";
+
+import { LoaderCircle } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+export default function SuggestMissingTasks({
+  projectId,
+}: {
+  projectId: Id<"projects">;
+}) {
+  const [isLoadingSuggestMissingTasks, setIsLoadingSuggestMissingTasks] =
+    useState(false);
+
+  const suggestMissingTasks = useAction(api.suggestTasks.generateContent) || [];
+
+  const handleMissingTasks = async () => {
+    setIsLoadingSuggestMissingTasks(true);
+
+    try {
+      await suggestMissingTasks({ projectId: projectId[0] as Id<"projects"> });
+    } catch (error) {
+      console.log("Error in suggestingMissingTasks", error);
+    } finally {
+      setIsLoadingSuggestMissingTasks(false);
+    }
+  };
+
+  return (
+    <>
+      <Button
+        variant={"outline"}
+        disabled={isLoadingSuggestMissingTasks}
+        onClick={handleMissingTasks}
+      >
+        {isLoadingSuggestMissingTasks ? (
+          <div className="flex gap-2">
+            Loading Tasks (AI)
+            <LoaderCircle className="h-5 w-5 text-primary animate-spin" />
+          </div>
+        ) : (
+          "Suggest Missing Tasks (AI)"
+        )}
+      </Button>
+    </>
+  );
+}

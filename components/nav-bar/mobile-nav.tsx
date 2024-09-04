55c500c7f71b0bb9bb3593ef8d01567b1973dc07
Shubham Bhardwaj <shubhardwaj10@gmail.com>
2024-09-04 12:06:00 +0530

    feat: Add Dialog component for creating projects and tasks

--- components/nav-bar/mobile-nav.tsx
+++ components/nav-bar/mobile-nav.tsx
@@ -20,85 +20,90 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { CircleUser, Menu, Search } from "lucide-react";
+import { CircleUser, Menu, PlusIcon, Search } from "lucide-react";
+import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog";
 
 export default function MobileNav() {
   return (
-    <div className="flex flex-col">
-      <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button
-              variant="outline"
-              size="icon"
-              className="shrink-0 md:hidden"
-            >
-              <Menu className="h-5 w-5" />
-              <span className="sr-only">Toggle navigation menu</span>
-            </Button>
-          </SheetTrigger>
-          <SheetContent side="left" className="flex flex-col">
-            <nav className="grid gap-2 text-lg font-medium">
-              <UserProfile />
+    <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button variant="outline" size="icon" className="shrink-0 md:hidden">
+            <Menu className="h-5 w-5" />
+            <span className="sr-only">Toggle navigation menu</span>
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left" className="flex flex-col">
+          <nav className="grid gap-2 text-lg font-medium">
+            <UserProfile />
 
-              {primaryNavItems.map(({ icon, name, link }, index) => (
-                <Link
-                  key={index}
-                  href={link}
-                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-                >
-                  {icon}
-                  {name}
-                </Link>
-              ))}
-            </nav>
-            <div className="mt-auto">
-              <Card>
-                <CardHeader>
-                  <CardTitle>Upgrade to Pro</CardTitle>
-                  <CardDescription>
-                    Unlock all features and get unlimited access to our support
-                    team.
-                  </CardDescription>
-                </CardHeader>
-                <CardContent>
-                  <Button size="sm" className="w-full">
-                    Upgrade
-                  </Button>
-                </CardContent>
-              </Card>
-            </div>
-          </SheetContent>
-        </Sheet>
-        <div className="w-full flex-1">
-          <form>
-            <div className="relative">
-              <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-              <Input
-                type="search"
-                placeholder="Search products..."
-                className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
-              />
-            </div>
-          </form>
-        </div>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="secondary" size="icon" className="rounded-full">
-              <CircleUser className="h-5 w-5" />
-              <span className="sr-only">Toggle user menu</span>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Settings</DropdownMenuItem>
-            <DropdownMenuItem>Support</DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Logout</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </header>
-    </div>
+            {primaryNavItems.map(({ icon, name, link }, index) => (
+              <Link
+                key={index}
+                href={link}
+                className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+              >
+                {icon}
+                {name}
+              </Link>
+            ))}
+
+            <Dialog>
+              <DialogTrigger id="closeDialog">
+                <p className="flex justify-between items-center">
+                  My Projects
+                  <PlusIcon className="h-5 w-5" aria-label={"Add a Project"} />
+                </p>
+              </DialogTrigger>
+              <DialogContent>Hello</DialogContent>
+            </Dialog>
+          </nav>
+          <div className="mt-auto">
+            <Card>
+              <CardHeader>
+                <CardTitle>Upgrade to Pro</CardTitle>
+                <CardDescription>
+                  Unlock all features and get unlimited access to our support
+                  team.
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <Button size="sm" className="w-full">
+                  Upgrade
+                </Button>
+              </CardContent>
+            </Card>
+          </div>
+        </SheetContent>
+      </Sheet>
+      <div className="w-full flex-1">
+        <form>
+          <div className="relative">
+            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+            <Input
+              type="search"
+              placeholder="Search products..."
+              className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
+            />
+          </div>
+        </form>
+      </div>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="secondary" size="icon" className="rounded-full">
+            <CircleUser className="h-5 w-5" />
+            <span className="sr-only">Toggle user menu</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Settings</DropdownMenuItem>
+          <DropdownMenuItem>Support</DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Logout</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </header>
   );
 }
--- components/nav-bar/side-bar.tsx
+++ components/nav-bar/side-bar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import UserProfile from "./user-profile";
 import { primaryNavItems } from "@/utils";
@@ -10,44 +12,109 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useEffect, useState } from "react";
+import { Doc } from "@/convex/_generated/dataModel";
+import { Hash, PlusIcon } from "lucide-react";
+import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog";
+
+type ListTitleType = {
+  [key: string]: string;
+};
 
 export default function SideBar() {
+  const pathName = usePathname();
+
+  const LIST_OF_TITLE_ID: ListTitleType = {
+    primary: "",
+    projects: "My Projects",
+  };
+
+  const projectList = useQuery(api.projects.getProjects);
+
+  const [navItems, setNavItems] = useState([...primaryNavItems]);
+
+  const renderItems = (projectList: Array<Doc<"projects">>) => {
+    return projectList.map(({ name, _id }, index) => {
+      return {
+        ...(index === 0 && { id: "projects" }),
+        name,
+        link: `/loggedin/projects/${_id}`,
+        icon: <Hash className="h-4 w-4" />,
+      };
+    });
+  };
+
+  useEffect(() => {
+    if (projectList) {
+      const projectItems = renderItems(projectList);
+      const items = [...primaryNavItems, ...projectItems];
+      setNavItems(items);
+    }
+  }, [projectList]);
+
   return (
     <div className="hidden border-r bg-muted/40 md:block">
       <div className="flex h-full max-h-screen flex-col gap-2">
-        <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+        <div className="flex justify-between h-14 items-center border-b p-1 lg:h-[60px] lg:px-2">
           <UserProfile />
         </div>
-        <div className="flex-1">
-          <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-            {primaryNavItems.map(({ icon, name, link }, index) => (
+
+        <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+          {navItems.map(({ icon, name, link, id }, index) => (
+            <div key={index}>
+              {id && (
+                <div className="flex items-center mt-6 mb-2">
+                  <p className="flex flex-1 text-base">
+                    {LIST_OF_TITLE_ID[id]}
+                  </p>
+                  {LIST_OF_TITLE_ID[id] === "My Projects" && (
+                    <Dialog>
+                      <DialogTrigger id="closeDialog">
+                        <PlusIcon
+                          className="h-5 w-5"
+                          aria-label={"Add a Project"}
+                        />
+                      </DialogTrigger>
+                      <DialogContent>Hello</DialogContent>
+                    </Dialog>
+                  )}
+                </div>
+              )}
               <Link
                 key={index}
                 href={link}
-                className="flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary"
+                className={cn(
+                  "flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary",
+                  pathName === link
+                    ? "active rounded-lg bg-primary/10 text-primary transition-all hover:text-primary"
+                    : "text-foreground"
+                )}
               >
                 {icon}
                 {name}
               </Link>
-            ))}
-          </nav>
-        </div>
-        <div className="mt-auto p-4">
-          <Card x-chunk="dashboard-02-chunk-0">
-            <CardHeader className="p-2 pt-0 md:p-4">
-              <CardTitle>Upgrade to Pro</CardTitle>
-              <CardDescription>
-                Unlock all features and get unlimited access to our support
-                team.
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="p-2 pt-0 md:p-4 md:pt-0">
-              <Button size="sm" className="w-full">
-                Upgrade
-              </Button>
-            </CardContent>
-          </Card>
-        </div>
+            </div>
+          ))}
+        </nav>
+      </div>
+      <div className="mt-auto p-4">
+        <Card x-chunk="dashboard-02-chunk-0">
+          <CardHeader className="p-2 pt-0 md:p-4">
+            <CardTitle>Upgrade to Pro</CardTitle>
+            <CardDescription>
+              Unlock all features and get unlimited access to our support team.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="p-2 pt-0 md:p-4 md:pt-0">
+            <Button size="sm" className="w-full">
+              Upgrade
+            </Button>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );

b0c0f7649ed9b6efcf78e352bbf94394cb530bbb
Shubham Bhardwaj <shubhardwaj10@gmail.com>
2024-09-10 10:40:54 +0530

    refactor: Add deleteProjectById and deleteProject functions

--- convex/projects.ts
+++ convex/projects.ts
@@ -1,6 +1,7 @@
 import { v } from "convex/values";
-import { mutation, query } from "./_generated/server";
+import { action, mutation, query } from "./_generated/server";
 import { handleUserId } from "./auth";
+import { api } from "./_generated/api";
 
 export const getProjects = query({
   args: {},
@@ -66,6 +67,60 @@ export const createAProject = mutation({
       return null;
     } catch (error) {
       console.log("Error occurred during createAProject mutation");
+
+      return null;
+    }
+  },
+});
+
+export const deleteProjectById = mutation({
+  args: {
+    projectId: v.id("projects"),
+  },
+  handler: async (ctx, { projectId }) => {
+    try {
+      const userId = await handleUserId(ctx);
+
+      if (userId) {
+        const deletedProjectId = await ctx.db.delete(projectId);
+
+        return deletedProjectId;
+      }
+    } catch (error) {
+      console.log("Error occurred during deleteProjectById mutation");
+
+      return null;
+    }
+  },
+});
+
+export const deleteProject = action({
+  args: {
+    projectId: v.id("projects"),
+  },
+  handler: async (ctx, { projectId }) => {
+    try {
+      const userId = await handleUserId(ctx);
+
+      if (userId) {
+        const allTodos = await ctx.runQuery(api.todos.getTodosByProjectId, {
+          projectId,
+        });
+
+        const statuses = await Promise.allSettled(
+          allTodos.map(({ _id }) =>
+            ctx.runMutation(api.todos.deleteTodo, {
+              todoId: _id,
+            })
+          )
+        );
+
+        await ctx.runMutation(api.projects.deleteProjectById, {
+          projectId,
+        });
+      }
+    } catch (error) {
+      console.log("Error occurred during deleteProject mutation");
 
       return null;
     }
